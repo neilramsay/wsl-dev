5077e960253e1eb9ac969017c0c862408204c0da
Neil Ramsay <2934552+neilramsay@users.noreply.github.com>
2024-11-18 12:26:22 +1300

    feat: Add Ansible bootstrap

---
+++ bootstrap.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -ex
+
+PROJECT=${1:-"wsl-dev"}
+
+sudo apt-get update &&
+sudo apt-get install python3-venv &&
+
+git clone https://github.com/neilramsay/${PROJECT}.git &&
+cd ${PROJECT} &&
+python3 -m venv .venv &&
+source .venv/bin/activate &&
+pip install --require-virtualenv --requirement requirements.txt &&
+
+ansible-playbook playbook.yaml
