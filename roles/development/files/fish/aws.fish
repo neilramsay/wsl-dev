7c652c958dd07af7f436ee576e0e04199565c8c4
Neil Ramsay <2934552+neilramsay@users.noreply.github.com>
2025-02-28 16:32:59 +1300

    feat: Update AWS fish completer

--- roles/development/files/fish/aws.fish
+++ roles/development/files/fish/aws.fish
@@ -1,7 +1,15 @@
 # https://stackoverflow.com/questions/26981542/aws-cli-command-completion-with-fish-shell
 
 function __fish_complete_aws
-    env COMP_LINE=(commandline -pc) aws_completer | tr -d ' '
+    env COMP_LINE=(commandline --current-process --cut-at-cursor) aws_completer | tr -d ' '
 end
 
-complete -c aws -f -a "(__fish_complete_aws)"
+function __fish_complete_aws_profiles
+    if test -f ~/.aws/config;
+        grep --no-messages --only-matching --colour=never --perl-regexp '(?<=\[profile )(.+)(?=\])' ~/.aws/config
+    end
+end
+
+complete --command aws --no-files --arguments "(__fish_complete_aws)"
+complete --command set --condition "__fish_is_first_token" --arguments "AWS_PROFILE"
+complete --command set --condition "__fish_contains_opt -s x and __fish_seen_subcommand_from AWS_PROFILE" --no-files --arguments "(__fish_complete_aws_profiles)"
