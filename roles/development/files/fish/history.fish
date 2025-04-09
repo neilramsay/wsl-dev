1518fa7c90bb9858c13cb212d3dcec9938181ef2
Neil Ramsay <2934552+neilramsay@users.noreply.github.com>
2025-04-09 13:18:21 +1200

    fix: Fixup history completion

--- roles/development/files/fish/history.fish
+++ roles/development/files/fish/history.fish
@@ -1,3 +1,7 @@
 function history
-    builtin history --show-time="%F %T - " $argv
+    if test $(count $argv) -eq 0 -o "$argv[1]" = "search"
+        builtin history --show-time="%F %T - " $argv
+    else
+        builtin history $argv
+    end
 end
